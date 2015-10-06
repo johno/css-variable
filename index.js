f0cb549de28dc0ad18009b46f70de5c788859cfc
John Otander <johnotander@gmail.com>
2015-10-06 12:36:16 -0600

    Add sass check and strip functions

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 'use strict'
 
 var isCustomProperty = require('./util/is-custom-property')
+var isSassVariable = require('./util/is-sass-variable')
 var isLessVariable = require('./util/is-less-variable')
 var stripCustomPropertySyntax = require('./util/strip-custom-property-syntax')
+var stripSassSyntax = require('./util/strip-sass-syntax')
 var stripLessSyntax = require('./util/strip-less-syntax')
 
 var CssVariable = function (variable) {
@@ -20,8 +22,10 @@ var CssVariable = function (variable) {
 
 CssVariable.prototype = {
   isCustomProperty: isCustomProperty,
+  isSassVariable: isSassVariable,
   isLessVariable: isLessVariable,
   stripCustomPropertySyntax: stripCustomPropertySyntax,
+  stripSassSyntax: stripSassSyntax,
   stripLessSyntax: stripLessSyntax
 }
 
--- test/test.js
+++ test/test.js
@@ -19,6 +19,20 @@ test('strip-custom-property-syntax', function (t) {
   t.equal(cssVariable().stripCustomPropertySyntax('blah--blah'), 'blah--blah')
 })
 
+test('is-sass-variable', function(t) {
+  t.plan(2)
+
+  t.ok(cssVariable().isSassVariable('$foo'))
+  t.ok(!cssVariable().isSassVariable('blah'))
+})
+
+test('strip-sass-syntax', function(t) {
+  t.plan(2)
+
+  t.equal(cssVariable().stripSassSyntax('$foo'), 'foo')
+  t.equal(cssVariable().stripSassSyntax('blah'), 'blah')
+})
+
 test('is-less-variable', function(t) {
   t.plan(2)
 
